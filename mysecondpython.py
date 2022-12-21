db982fcb7a362acfe1e764107154580eaa14e6a9
Lawry36 <120947783+Lawry36@users.noreply.github.com>
2022-12-21 10:21:14 +0000

    Add files via upload

---
+++ mysecondpython.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupiter Notebook on IBM Watson Studion
+
+# **Lawrence Opoku**
+# 
+# Machine Learning Engineer
+
+# *I am interested in data science because I love solving problems with insights drawn from data*
+
+# ### My code is suppposed to return the sum of a and b
+
+# In[3]:
+
+
+a = 5
+b = 8
+
+print(a + b)
+
+
+# <a href = "https://desktop.github.com/">Github Desktop</a>
+# 
+# | Pencil | Biscuit |
+# | ------ | ------ |
+# | 5 | 3 |
+# 
+# 
+# > Data Science is what data scientists do.
+# 
+# 
+# * Mango 
+# * Banana 
+# * Grapes
+# * Pineapple
+# * Watermelon
+# 
+# ~~Python is an object oriented programming language~~
+# 
+# ***
+# 
+# ![python%20programming.jpeg](attachment:python%20programming.jpeg)
+# 
+# 
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
